e5a9587be94534ec3341d2c7b0192ae267287b1b
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2026-09-02 01:02:04 +0800

    minor: progress

--- pm/lib.v
+++ pm/lib.v
@@ -2,23 +2,40 @@ Inductive Term := .
 
 Inductive Op := .
 
-Definition PM_Prop (n : nat) : Prop. Admitted.
-Example test_prop : PM_Prop 1. Admitted.
+Definition Order (n : nat) : Prop. Admitted.
+Example test_prop : Order 1. Admitted.
 
-Definition PM_and {n : nat} (X Y : PM_Prop n) : PM_Prop n. Admitted.
-Example test_and (X Y : PM_Prop 1) := PM_and X Y.
-Example test_and_1 (X Y Z : PM_Prop 1) := PM_and X (PM_and Y Z).
+Definition PM_and {n : nat} (X Y : Order n) : Order n. Admitted.
+Example test_and (X Y : Order 1) := PM_and X Y.
+Example test_and_1 (X Y Z : Order 1) := PM_and X (PM_and Y Z).
 
-Definition PM_imp {n : nat} (X Y : PM_Prop n) : PM_Prop n. Admitted.
+Definition PM_imp {n : nat} (X Y : Order n) : Order n. Admitted.
 
-Definition PM_asserted {n : nat} (X : PM_Prop n) : Prop. Admitted.
+Definition PM_asserted {n : nat} (X : Order n) : Prop. Admitted.
 
-Theorem test_axiom (X Y Z : PM_Prop 1) : 
+Theorem test_axiom (X Y Z : Order 1) : 
   PM_asserted
     (PM_imp (PM_and X (PM_and Y Z)) (PM_and X Y)).
   Admitted.
 
+Theorem test_impl {n : nat} (X Y Z : Order n) :
+  PM_asserted (PM_imp X Y)
+    -> PM_asserted X -> PM_asserted Y. 
+  Admitted.
+
+Theorem test_proof (X Y Z : Order 1) :
+  PM_asserted 
+    (PM_imp (PM_and X Y) (PM_and X Y)).
+Proof.
+  (* TODO: write something... *)
+Admitted.
 (* TODO: 
 - when rewrite on a theorem, unbox the pm_asserted 
 - when boxing/unboxing the theorem, try to preserve the order information
 *)
+
+(* 
+- Should we only shift asserted propositions or directly on propositions? 
+- Should we design as a tactic or a function?
+*)
+Definition shift : Prop. Admitted.
