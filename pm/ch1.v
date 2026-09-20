af61e0ccb74e2d8026835f987dcebc9f3c5dbe0a
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2026-09-20 11:24:16 +0800

    minor: progress

--- pm/ch1.v
+++ pm/ch1.v
@@ -2,6 +2,7 @@ Require Import PM.pm.lib.
 
 (* 
 TODO: 
-- (*1.7) Design the predicate "is elementary proposition"
+- (*1.7) Design the predicate "is elementary proposition" for Order type. 
+  Should we change `Order` type into a module?
 - (*1.72) Design the predicate "is elementary propositional functions"
 *)
